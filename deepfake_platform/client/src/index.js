6dc1f98f580b765f4ac65ee11e7758ae68afd070
이병헌 <dlvkdlee@ibyeongheon-ui-MacBookPro.local>
2023-10-13 15:08:57 +0900

    test: github page deploy test2

--- deepfake_platform/client/src/index.js
+++ deepfake_platform/client/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
