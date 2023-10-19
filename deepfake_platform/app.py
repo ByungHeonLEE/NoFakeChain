ab15a3cd972d7dd1318c7528188ca30fa28550f8
lukepark327 <lukepark327@gmail.com>
2023-10-19 16:52:53 +0900

    feat: ipfs

--- deepfake_platform/app.py
+++ deepfake_platform/app.py
@@ -1,6 +1,7 @@
+import requests
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
-import os 
+import os
 from dotenv import load_dotenv
 from deepfake_detection.azimuthal import azimuthalAverage
 
@@ -13,6 +14,7 @@ allowed_origins = os.environ.get("ALLOWED_ORIGINS", "").split(",")
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
+
 
 def get_deepfake_result(file_path):
     return False
@@ -27,18 +29,18 @@ def upload_image():
     data_key = "image"
     if data_key not in request.files:
         return jsonify({"error": "No Image file provided"}), 400
-    
+
     file = request.files[data_key]
     if request.files[data_key].filename == '':
         return jsonify({"error": "No Image file selected"}), 400
-    
+
     mimetype = file.mimetype.split("/")[0]
-    if not mimetype in ["image","video"]:
+    if not mimetype in ["image", "video"]:
         return jsonify({"error": "No Image file selected"}), 400
 
     # save on local storage
     rel_path = db.get_relative_path(file.filename)
-    file_path = os.path.join(db.get_store_path(),rel_path)
+    file_path = os.path.join(db.get_store_path(), rel_path)
     file.save(file_path)
 
     # assign data info
@@ -47,12 +49,12 @@ def upload_image():
 
     # Call the deepfake detection function
     is_deepfake = get_deepfake_result(file_path)
-    
+
     # Save Image reference to MongoDB
     image_data = {
         "title": title,
         "description": description,
-        "path": os.path.join(db.ARTICLE_PATH,rel_path),
+        "path": os.path.join(db.ARTICLE_PATH, rel_path),
         "image_or_video": mimetype,
         "is_deepfake": is_deepfake
     }
@@ -81,14 +83,33 @@ def serve_images(filename):
 
 @app.route('/classify', methods=['POST'])
 def classify():
-    if 'image' not in request.files:
-        return jsonify({"error": "No image file provided."}), 400
+    data = request.get_json()
 
-    image = request.files['image']
-    image = image_detector.refine_image(image)
-    result = image_detector.classify_image(image)
+    if 'hash' not in data:
+        return jsonify({"error": "No IPFS hash provided."}), 400
+
+    hash = data['hash']
+    image_data = get_image_from_ipfs(hash)
+
+    if not image_data:
+        return jsonify({"error": "Failed to fetch image from IPFS."}), 400
+
+    refined_image = image_detector.refine_image(image_data)
+    result = image_detector.classify_image(refined_image)
 
     return jsonify({"result": result})
+
+
+def get_image_from_ipfs(hash):
+    """Fetch image from IPFS given its hash."""
+    IPFS_GATEWAY = "https://ipfs.io/ipfs/"
+
+    try:
+        response = requests.get(IPFS_GATEWAY + hash, timeout=10)
+        response.raise_for_status()
+        return response.content
+    except requests.RequestException:
+        return None
 
 
 if __name__ == '__main__':
